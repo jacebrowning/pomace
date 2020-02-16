2ada84ed356973071769bb1add8c6c5fff023009
Jace Browning <jacebrowning@gmail.com>
2020-02-16 17:23:50 -0500

    Escape path slashes

--- pomelo/tests/test_models.py
+++ pomelo/tests/test_models.py
@@ -3,6 +3,22 @@
 from .. import models
 
 
+def describe_url():
+    def describe_init():
+        def it_removes_escapted_path_characters(expect, monkeypatch):
+            monkeypatch.setattr(models.URL, 'SLASH', '@')
+            url = models.URL('www.example.com', '@foo@bar')
+            expect(url.domain) == 'www.example.com'
+            expect(url.path) == '/foo/bar'
+
+    def describe_path_encoded():
+        def it_replaces_paths_slashes_with_a_special_character(expect, monkeypatch):
+            monkeypatch.setattr(models.URL, 'SLASH', '@')
+            url = models.URL('http://www.example.com/foo/bar')
+            expect(url.domain) == 'www.example.com'
+            expect(url.path_encoded) == '@foo@bar'
+
+
 def describe_action():
     def describe_str():
         def default(expect):
