91a0ce5fb7361ca6a6ced60fe6bf895ef462c068
Jace Browning <jacebrowning@gmail.com>
2022-08-14 12:04:14 -0400

    Default to lowercase on CI

--- pomace/tests/test_prompts.py
+++ pomace/tests/test_prompts.py
@@ -8,7 +8,7 @@ def describe_framework_if_unset():
         monkeypatch.setenv("CI", "true")
         config.settings.framework = ""
         prompts.framework_if_unset()
-        expect(config.settings.framework) == "Splinter"
+        expect(config.settings.framework) == "splinter"
 
 
 def describe_browser_if_unset():
