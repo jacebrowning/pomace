d8776a08807167527b204e1cf28717c5e3fe16ec
Jace Browning <jacebrowning@gmail.com>
2022-08-17 17:04:19 -0400

    Test that Playwright is optional

--- pomace/compat.py
+++ pomace/compat.py
@@ -1,15 +1,27 @@
+"""Compatibility layer to handle systems that cannot run Playwright."""
+
+# pylint: disable=unused-import
+# mypy: ignore-errors
+
+import sys
+
 try:
-    # pylint: disable=unused-import
+
     from playwright.sync_api import Browser as PlaywrightBrowser
     from playwright.sync_api import ElementHandle
     from playwright.sync_api import ElementHandle as PlaywrightElement
     from playwright.sync_api import Error as PlaywrightError
     from playwright.sync_api import Page
     from playwright.sync_api import sync_playwright as playwright
+
+    if "pytest" in sys.modules:
+        raise ImportError
+
 except ImportError:
-    PlaywrightBrowser = None  # type: ignore
-    ElementHandle = None  # type: ignore
-    PlaywrightElement = None  # type: ignore
-    PlaywrightError = None  # type: ignore
-    Page = None  # type: ignore
-    playwright = None  # type: ignore
+
+    PlaywrightBrowser = None
+    ElementHandle = None
+    PlaywrightElement = None
+    PlaywrightError = None
+    Page = None
+    playwright = None
