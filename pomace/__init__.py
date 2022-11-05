42c31ee35104044834ea81da017b7c221bde718d
Jace Browning <jacebrowning@gmail.com>
2022-11-05 13:05:54 -0400

    Switch to importlib for version detection

--- pomace/__init__.py
+++ pomace/__init__.py
@@ -1,10 +1,12 @@
-from pkg_resources import DistributionNotFound as _DistributionNotFound
-from pkg_resources import get_distribution as _get_distribution
+from importlib.metadata import PackageNotFoundError, version
 
 from .api import *
 from .config import settings
 
 try:
-    __version__ = _get_distribution("pomace").version
-except _DistributionNotFound:
+    __version__ = version("pomace")
+except PackageNotFoundError:
     __version__ = "(local)"
+
+del PackageNotFoundError
+del version
